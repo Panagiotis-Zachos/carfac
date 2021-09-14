9153d5277252d8d4abea0d588de5642492cc3eb7
Panagiotis Zachos <panzaxos@gmail.com>
2021-09-14 12:25:49 +0300

    Update commited files to work with new MATLAB. Custom channel number tests

--- matlab/CARFAC_binaural.m
+++ matlab/CARFAC_binaural.m
@@ -18,21 +18,35 @@
 
 % Test/demo hacking for carfac matlab stuff, two-channel (binaural) case
 
-clear variables
+clearvars
 
-agc_plot_fig_num = 1;
+agc_plot_fig_num = 0;
 
-tic
-
-file_signal = audioread('../test_data/binaural_test.wav');
+[file_signal,fsOld] = audioread('../test_data/binaural_test.wav');
 file_signal = file_signal(9000+(1:15000));  % trim for a faster test
 
 itd_offset = 22;  % about 1 ms
 test_signal = [file_signal((itd_offset+1):end), ...
                file_signal(1:(end-itd_offset))] / 10;
 
-n_ears = 2;           
-CF_struct = CARFAC_Design(n_ears);  % default design
+fs = 16000;
+test_signal = resample(test_signal,fs,fsOld);
+n_ears = 2;
+
+
+CF_CAR_params = struct( ...
+    'velocity_scale', 0.1, ...  % for the velocity nonlinearity
+    'v_offset', 0.04, ...  % offset gives a quadratic part
+    'min_zeta', 0.10, ... % minimum damping factor in mid-freq channels
+    'max_zeta', 0.35, ... % maximum damping factor in mid-freq channels
+    'first_pole_theta', 1*pi, ...% Default:0.85*pi, ...
+    'zero_ratio', sqrt(2), ... % how far zero is above pole
+    'high_f_damping_compression', 0.5, ... % 0 to 1 to compress zeta
+    'ERB_per_step', 0.5, ... % assume G&M's ERB formula
+    'min_pole_Hz', 500, ... % Default: 30
+    'ERB_break_freq', 165.3, ...  % Greenwood map's break freq.
+    'ERB_Q', 1000/(24.7*4.37));  % Glasberg and Moore's high-cf ratio
+CF_struct = CARFAC_Design(n_ears,fs,CF_CAR_params);  % default design
 
 % Run stereo test:
 
@@ -41,20 +55,18 @@ CF_struct = CARFAC_Init(CF_struct);
 [CF_struct, nap_decim, nap] = CARFAC_Run(CF_struct, test_signal, agc_plot_fig_num);
 
 % Display results for 2 ears:
-for ear = 1:n_ears
-  smooth_nap = nap_decim(:, :, ear);
-  figure(ear + n_ears)  % Makes figures 3 and 4
-  image(63 * ((abs(smooth_nap))' .^ 0.5))
-
-  colormap(1 - gray);
-end
-
-toc
+% for ear = 1:n_ears
+%   smooth_nap = nap_decim(:, :, ear);
+%   figure(ear + n_ears)  % Makes figures 3 and 4
+%   image(63 * ((abs(smooth_nap))' .^ 0.5))
+% 
+%   colormap(1 - gray);
+% end
 
 % Show resulting data, even though M-Lint complains:
-CF_struct
-CF_struct.CAR_params
-CF_struct.AGC_params
-min_max = [min(nap(:)), max(nap(:))]
-min_max_decim = [min(nap_decim(:)), max(nap_decim(:))]
+% CF_struct
+% CF_struct.CAR_params
+% CF_struct.AGC_params
+% min_max = [min(nap(:)), max(nap(:))]
+% min_max_decim = [min(nap_decim(:)), max(nap_decim(:))]
 
--- matlab/local_functions/CARFAC_Design.m
+++ matlab/local_functions/CARFAC_Design.m
@@ -56,11 +56,11 @@ if nargin < 3
     'v_offset', 0.04, ...  % offset gives a quadratic part
     'min_zeta', 0.10, ... % minimum damping factor in mid-freq channels
     'max_zeta', 0.35, ... % maximum damping factor in mid-freq channels
-    'first_pole_theta', 0.85*pi, ...
+    'first_pole_theta', 0.85*pi, ...% Default:0.85*pi, ...
     'zero_ratio', sqrt(2), ... % how far zero is above pole
     'high_f_damping_compression', 0.5, ... % 0 to 1 to compress zeta
     'ERB_per_step', 0.5, ... % assume G&M's ERB formula
-    'min_pole_Hz', 30, ...
+    'min_pole_Hz', 30, ... % Default: 30
     'ERB_break_freq', 165.3, ...  % Greenwood map's break freq.
     'ERB_Q', 1000/(24.7*4.37));  % Glasberg and Moore's high-cf ratio
 end
